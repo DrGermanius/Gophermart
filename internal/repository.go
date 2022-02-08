cf857ead9cb60c6bb28bdf568c1ad9ba9242b417
DrGermanius <samolazov.herman@gmail.com>
2022-02-09 00:55:53 +0300

    test

--- internal/repository.go
+++ internal/repository.go
@@ -42,39 +42,39 @@ func NewRepository(connString string) (*Repository, error) {
 }
 
 func createDatabaseAndTable(c *pgxpool.Pool) error {
-	_, err := c.Exec(context.Background(), "CREATE DATABASE mart")
-	if err != nil {
-		return err
-	}
-	_, err = c.Exec(context.Background(), `CREATE TABLE users
-	(
-		id       SERIAL PRIMARY KEY,
-		login    VARCHAR(255)    NOT NULL,
-		password VARCHAR(255)    NOT NULL,
-		balance  DECIMAL(36, 18) NOT NULL DEFAULT 0.0
-	);
-
-	CREATE TABLE orders
-	(
-		id          SERIAL PRIMARY KEY,
-		number      BIGINT UNIQUE   NOT NULL,
-		user_id     INT             NOT NULL REFERENCES users,
-		accrual     DECIMAL(36, 18) NOT NULL DEFAULT 0.0,
-		status      VARCHAR(255)    NOT NULL,
-		uploaded_at TIMESTAMP       NOT NULL
-	);
-
-	CREATE TABLE withdraw_history
-	(
-		id           SERIAL PRIMARY KEY,
-		order_number BIGINT          NOT NULL REFERENCES orders (number),
-		user_id      INT             NOT NULL REFERENCES users,
-		amount       DECIMAL(36, 18) NOT NULL DEFAULT 0.0,
-		withdrawn    DECIMAL(36, 18) NOT NULL DEFAULT 0.0
-	);`)
-	if err != nil {
-		return err
-	}
+	//_, err := c.Exec(context.Background(), "CREATE DATABASE mart")
+	//if err != nil {
+	//	return err
+	//}
+	//_, err = c.Exec(context.Background(), `CREATE TABLE users
+	//(
+	//	id       SERIAL PRIMARY KEY,
+	//	login    VARCHAR(255)    NOT NULL,
+	//	password VARCHAR(255)    NOT NULL,
+	//	balance  DECIMAL(36, 18) NOT NULL DEFAULT 0.0
+	//);
+	//
+	//CREATE TABLE orders
+	//(
+	//	id          SERIAL PRIMARY KEY,
+	//	number      BIGINT UNIQUE   NOT NULL,
+	//	user_id     INT             NOT NULL REFERENCES users,
+	//	accrual     DECIMAL(36, 18) NOT NULL DEFAULT 0.0,
+	//	status      VARCHAR(255)    NOT NULL,
+	//	uploaded_at TIMESTAMP       NOT NULL
+	//);
+	//
+	//CREATE TABLE withdraw_history
+	//(
+	//	id           SERIAL PRIMARY KEY,
+	//	order_number BIGINT          NOT NULL REFERENCES orders (number),
+	//	user_id      INT             NOT NULL REFERENCES users,
+	//	amount       DECIMAL(36, 18) NOT NULL DEFAULT 0.0,
+	//	withdrawn    DECIMAL(36, 18) NOT NULL DEFAULT 0.0
+	//);`)
+	//if err != nil {
+	//	return err
+	//}
 	return nil
 }
 
