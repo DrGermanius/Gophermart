07a50f55a83f444f540909e7008ac151d680e2be
DrGermanius <samolazov.herman@gmail.com>
2022-02-09 20:55:53 +0300

    test

--- internal/handlers.go
+++ internal/handlers.go
@@ -171,7 +171,7 @@ func (h *Handlers) WithdrawHistory(c *fiber.Ctx) error {
 func setAuthCookie(c *fiber.Ctx, token string) {
 	cookie := &fiber.Cookie{
 		Name:    "token",
-		Value:   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJleHAiOjE2",
+		Value:   token,
 		Path:    "/",
 		MaxAge:  100,
 		Expires: time.Now().Add(24 * time.Hour),
